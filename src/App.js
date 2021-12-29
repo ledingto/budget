54283ddac4c02d9a8ca63c19da9e496d6007c5af
Laura Edington <edington.laura@gmail.com>
2021-12-28 18:32:38 -0600

    Basic example with styling using semantic and fomantic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,102 @@
+import { Container, Header, Statistic, Segment, Grid, Icon, Form, Button } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">App Start</div>
+    <Container>
+      <Header as='h1'>Budget</Header>
+
+      <Statistic size='small'>
+        <Statistic.Label>Your Balance:</Statistic.Label>
+        <Statistic.Value>2,550.00</Statistic.Value>
+      </Statistic>
+
+      <Segment textAlign="center">
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size="tiny" color="green">
+                <Statistic.Label>
+                  Income:
+                </Statistic.Label>
+                <Statistic.Value>1,000.00</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size="tiny" color="red">
+                <Statistic.Label>
+                  Expenses:
+                </Statistic.Label>
+                <Statistic.Value>600.00</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Header as="h3">History</Header>
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">Restaurant</Grid.Column>
+            <Grid.Column width={3} textAlign="right">10.00</Grid.Column>
+            <Grid.Column width={3} textAlign="center">
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">Grocery</Grid.Column>
+            <Grid.Column width={3} textAlign="right">44.00</Grid.Column>
+            <Grid.Column width={3} textAlign="center">
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color="green">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">Paycheck</Grid.Column>
+            <Grid.Column width={3} textAlign="right">2500.00</Grid.Column>
+            <Grid.Column width={3} textAlign="center">
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">Rent</Grid.Column>
+            <Grid.Column width={3} textAlign="right">1000.00</Grid.Column>
+            <Grid.Column width={3} textAlign="center">
+              <Icon name="edit" />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Header as="h3">Add New Transaction</Header>
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input icon="tags" width={12} label="Name" placeholder="New item" />
+          <Form.Input icon="dollar" iconPosition="left" width={4} label="Price" placeholder="0.00" />
+        </Form.Group>
+        <Button.Group style={{marginTop: 20}}>
+          <Button>Cancel</Button>
+          <Button.Or />
+          <Button primary>Save</Button>
+        </Button.Group>
+      </Form>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+/* import 'fomantic-ui-css/semantic.css'; */ // Using CDN in html instead
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
